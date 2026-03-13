a858888b4c573a6b7c632df7b3182f6b6527b49b
karanvirbasra011 <karanbasra91@gmail.com>
2026-03-13 13:51:18 +0530

    Refactor binary search implementation and cleanup

--- Week2/BinarySearch.cpp
+++ Week2/BinarySearch.cpp
@@ -1,76 +1,50 @@
 #include <iostream>
 #include <chrono>
-#include <iomanip>
-#include <algorithm>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 using namespace std;
 
-long long binarySearch_UEM243135(long long uem2_arr[], long long uem1_low, long long uem4_high, long long uem3_target)
-{
-    if (uem1_low > uem4_high) return -1;
-
-    long long uem1_mid = (uem1_low + uem4_high) / 2;
-
-    if (uem2_arr[uem1_mid] == uem3_target) return uem1_mid;
-    else if (uem2_arr[uem1_mid] > uem3_target) return binarySearch_UEM243135(uem2_arr, uem1_low, uem1_mid - 1, uem3_target);
-    else return binarySearch_UEM243135(uem2_arr, uem1_mid + 1, uem4_high, uem3_target);
+int binarySearch(int arr[], int low, int high, int target) {
+    if (low > high) return -1;
+    int mid = (low + high) / 2;
+    if (arr[mid] == target) return mid;
+    else if (arr[mid] > target) return binarySearch(arr, low, mid - 1, target);
+    else return binarySearch(arr, mid + 1, high, target);
 }
 
-int main()
-{
-    cout << "Roll No: UEM243135" << endl;
-    cout << "Binary Search - Time Analysis\n\n";
-
-    // UEM2 - array sizes | UEM4 - size count = 10
-    long long uem2_testSizes[10] = {1000, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000};
-    double   uem4_timings[10][10];
+int main() {
+    int testValues[10] = {1000, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000};
+    int runs = 10000;
 
     srand(time(0));
+    cout << "UEM243135 Code for Binary Search\n";
+    cout << "Array Size | Average Time (microseconds)\n";
+    cout << "------------------------------------------\n";
 
-    for (long long uem1_i = 0; uem1_i < 10; uem1_i++)
-    {
-        long long uem4_n = uem2_testSizes[uem1_i];
+    for (int i = 0; i < 10; i++) {
+        int n = testValues[i];
 
-        // UEM2 - dynamic plain array (no vectors)
-        long long *uem2_arr = new long long[uem4_n];
-
-        for (long long uem1_k = 0; uem1_k < uem4_n; uem1_k++) {
-            uem2_arr[uem1_k] = rand() % 1000 + 1;
+        int* arr = new int[n];
+        for (int k = 0; k < n; k++) {
+            arr[k] = k + 1;  // sorted array 1,2,3...n
         }
+        
+        int target = -1;
 
-        sort(uem2_arr, uem2_arr + uem4_n);
-
-        double   uem3_totalTime = 0;
-        long long uem1_result   = 0;
-
-        for (long long uem1_iter = 0; uem1_iter < 10; uem1_iter++)
-        {
-            auto uem1_startTime = chrono::high_resolution_clock::now();
-
-            uem1_result = binarySearch_UEM243135(uem2_arr, 0, uem4_n - 1, uem2_arr[0]);
-
-            auto uem1_endTime = chrono::high_resolution_clock::now();
-
-            chrono::duration<double, micro> uem1_duration = uem1_endTime - uem1_startTime;
-
-            uem4_timings[uem1_i][uem1_iter] = uem1_duration.count();
-            uem3_totalTime += uem1_duration.count();
+        double dummy = 0;
+        auto start = chrono::high_resolution_clock::now();
+        for (int r = 0; r < runs; r++) {
+            dummy += binarySearch(arr, 0, n - 1, target);
         }
+        auto end = chrono::high_resolution_clock::now();
 
-        if (uem1_result == -1) {
-            cout << "Element not found" << endl;
-        } else {
-            cout << "Element found at index: " << uem1_result << endl;
-        }
+        chrono::duration<double, micro> duration = end - start;
+        double avgTime = duration.count() / runs;
 
-        double uem3_avgTime = uem3_totalTime / 10.0;
+        cout << n << "      | " << avgTime << " microseconds\n";
 
-        cout << fixed << setprecision(4);
-        cout << "Array Size: " << uem4_n
-             << " | Average Time: " << uem3_avgTime << " microseconds\n\n";
-
-        delete[] uem2_arr;
+        delete[] arr;
     }
 
     return 0;
