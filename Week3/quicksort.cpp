ac4a3541853fdeda349e4c3e51af73af7208b324
karanvirbasra011 <karanbasra91@gmail.com>
2026-03-13 17:58:35 +0530

    Refactor quicksort to use arrays and measure performance
    Refactor quicksort implementation to use arrays instead of vectors, and add timing functionality for performance measurement.

--- Week3/quicksort.cpp
+++ Week3/quicksort.cpp
@@ -1,47 +1,69 @@
 #include <iostream>
-#include <vector>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-void swapElements(vector<int> &array, int firstIndex, int secondIndex) {
-    int temporaryValue = array[firstIndex];
-    array[firstIndex] = array[secondIndex];
-    array[secondIndex] = temporaryValue;
-}
+int partition(int A[], int p, int q) {
+    int x = A[p];     
+    int i = p;       
 
-int partition(vector<int> &array, int lowerBound, int upperBound) {
-    int pivotValue = array[upperBound];
-    int smallerElementIndex = lowerBound - 1;
-
-    for (int currentIndex = lowerBound; currentIndex <= upperBound - 1; currentIndex++) {
-        if (array[currentIndex] < pivotValue) {
-            smallerElementIndex++;
-            swapElements(array, smallerElementIndex, currentIndex);
+    for (int j = p + 1; j <= q; j++) {   
+        if (A[j] <= x) {                 
+            i = i + 1;                  
+            swap(A[i], A[j]);         
         }
     }
-
-    swapElements(array, smallerElementIndex + 1, upperBound);
-    return smallerElementIndex + 1;
+    swap(A[i], A[p]);  
+    return i;         
 }
 
-void quickSort(vector<int> &array, int lowerBound, int upperBound) {
-    if (lowerBound < upperBound) {
-        int pivotFinalPosition = partition(array, lowerBound, upperBound);
-
-        quickSort(array, lowerBound, pivotFinalPosition - 1);
-        quickSort(array, pivotFinalPosition + 1, upperBound);
+void quickSort(int A[], int p, int q) {
+    if (p < q) {
+        int pivot = partition(A, p, q);
+        quickSort(A, p, pivot - 1);
+        quickSort(A, pivot + 1, q);
     }
 }
 
 int main() {
-    vector<int> array = {10, 9, 10, 20, 20};
-    int totalElements = array.size();
+    int sizes[10] = {1000, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000};
+    int runs = 10;
 
-    quickSort(array, 0, totalElements - 1);
+    srand(time(0));
+    cout << "UEM243135 Code for QUICK SORT\n;
+    cout << "Array Size | Average Time (microseconds)\n";
+    cout << "------------------------------------------\n";
 
-    for (int i = 0; i < array.size(); i++) {
-        cout << array[i] << " ";
+    for (int i = 0; i < 10; i++) {
+        int n = sizes[i];
+
+        int* orig = new int[n];
+        for (int k = 0; k < n; k++)
+            orig[k] = rand() % 1000 + 1; 
+
+        double total = 0.0;
+
+        for (int r = 0; r < runs; r++) {
+            int* temp = new int[n];
+            for (int k = 0; k < n; k++)
+                temp[k] = orig[k];
+
+            auto start = chrono::high_resolution_clock::now();
+            quickSort(temp, 0, n - 1);
+            auto end = chrono::high_resolution_clock::now();
+
+            chrono::duration<double, micro> dur = end - start;
+            total += dur.count();
+
+            delete[] temp;
+        }
+
+        double avg = total / runs;
+        cout << n << "      | " << avg << " microseconds\n";
+
+        delete[] orig;
     }
-    cout << endl;
 
     return 0;
 }
